784aa1cc699f78d9d92758489d2b6459d2e0a33d
Yaroslav Kovalchuk <dantekip972@gmail.com>
2024-02-06 02:01:26 +0200

    add animation in home

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,126 @@
-<script setup>
-
-</script>
-
 <template>
-<div>Home</div>
+  <router-link to="/levels">
+    <section class="box_animate flex bg-gray-200">
+      <div class="box animate rotate">
+        <div class="front animate movefront" :style="{ background: frontColor }">
+        </div>
+        <div class="back animate moveback" :style="{ background: backColor }">
+        </div>
+      </div>
+    </section>
+
+  </router-link>
 </template>
 
 <style scoped>
+.box {
+  position: relative;
+  width: 500px;
+  height: 500px;
+  margin: 80px auto ;
+  transform-style: preserve-3d ;
+  animation-name: test ;
+  animation-duration: 15s;
+  animation-iteration-count: infinite;
+  animation-delay: 0s;
+}
+.front {
+  position: absolute;
+  width: 50%;
+  height: 50%;
+  background: green;
+  margin: auto;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+}
+.movefront{
+
+  animation-name: testZ ;
+  animation-duration: 15s;
+  animation-iteration-count: infinite;
+  animation-delay: 0s;
+}
+.front::before  {
+  position: absolute;
+  color:white;
+  top: 20px;
+  left: 120px;
+
+}
+.back {
+  position: absolute;
+  width: 50%;
+  height: 50%;
+  background: blue;
+  margin: auto ;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+}
+.moveback {
+  animation-name: testZ1 ;
+  animation-duration: 15s;
+  animation-iteration-count: infinite;
+  animation-delay: 0s;
+  /*animation-direction: reverse ; */
+}
+.back::before{
+  position: absolute;
+  color: white;
+  top: 20px;
+  left: 120px;
+  transform: rotateY(180deg);
+
+}
+
+@keyframes test {
+  0% {
+    transform: rotateY(0deg);
+  }
+  100% {
+    transform: rotateY(360deg);
+  }
+}
+@keyframes testZ {
+  0% {
+    transform: rotateZ(0deg);
+  }
+  100% {
+    transform: rotateZ(360deg);
+  }
+}
+@keyframes testZ1 {
+  0% {
+    transform: rotateZ(360deg) translateZ(-50px);
+  }
+  100% {
+    transform: rotateZ(0deg) translateZ(-50px);
+  }
+}
 
 </style>
+
+<script>
+export default {
+  data() {
+    return {
+      frontColor: 'green',
+      backColor: 'blue',
+    };
+  },
+  mounted() {
+    // Запускаем таймер для изменения цветов каждые 10 секунд
+    setInterval(this.changeColors, 15000);
+  },
+  methods: {
+    changeColors() {
+      // Изменяем цвета зеленого и синего на желтый и красный, и наоборот
+      this.frontColor = this.frontColor === 'green' ? 'yellow' : 'green';
+      this.backColor = this.backColor === 'blue' ? 'red' : 'blue';
+    },
+  },
+};
+</script>
