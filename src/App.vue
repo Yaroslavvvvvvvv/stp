6f36ced3f8696c001ffdc2f840f0ba0297de1ac2
Yaroslav Kovalchuk <dantekip972@gmail.com>
2024-02-07 16:55:01 +0200

    add footer

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 
 import MyHeader from "./components/MyHeader.vue";
+import MyFooter from "./components/MyFooter.vue";
 </script>
 
 <template>
   <MyHeader/>
   <router-view/>
+  <MyFooter/>
 </template>
 
 <style scoped>
