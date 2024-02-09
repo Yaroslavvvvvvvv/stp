9c17c04662e98ca08febc7cd2c29a22843c9d908
Yaroslav Kovalchuk <dantekip972@gmail.com>
2024-02-09 10:35:49 +0200

    add home content

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,31 +1,51 @@
 <template>
-  <div class="bg_images my_bg max-w-full">
-    <div>
-      <p class="text-white text-xl text-center"></p>
-    </div>
-  <router-link to="/levels" class="">
-    <div class="box_animate flex hover:scale-125 transition-transform duration-300">
-      <div class="box animate rotate">
-        <div class="front animate movefront flex animate-fade-down" :style="{ background: frontColor }">
-            <img src="/public/vite.svg" alt="" class="p-6">
-        </div>
-        <div class="back animate moveback flex" :style="{ background: backColor }">
-            <img src="/public/vite.svg" alt="" class="p-6">
+  <div class="bg-gray-200 max-w-full animate-fade-down animate-duration-1000">
+    <div class=" py-24 sm:py-32">
+      <div class="mx-auto max-w-7xl px-6 lg:px-8">
+        <div class="mx-auto grid max-w-2xl grid-cols-1 items-start gap-x-8 gap-y-16 sm:gap-y-24 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+          <div class="lg:pr-4">
+            <div class="relative overflow-hidden rounded-3xl bg-gray-900 px-6 pb-9 pt-64 shadow-2xl sm:px-12 lg:max-w-lg lg:px-8 lg:pb-8 xl:px-10 xl:pb-10">
+              <img class="absolute inset-0 h-full w-full object-cover brightness-125 saturate-0" src="/public/images/mybg.webp" alt="">
+              <div class="absolute left-1/2 top-1/2 -ml-16 -translate-x-1/2 -translate-y-1/2 transform-gpu blur-3xl" aria-hidden="true">
+                <div class="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-40" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
+              </div>
+            </div>
+          </div>
+          <div>
+            <div class="text-base leading-7 text-gray-700 lg:max-w-lg">
+              <h1 class="mt-2 text-2xl font-extrabold tracking-tight text-gray-950 sm:text-4xl">Basic Strength Program</h1>
+              <div class="max-w-xl">
+                <p class="mt-6 font-bold text-lg text-gray-600">- дозволяє стабільно прогресувати без перевантаження ЦНС та шкоди здоров'ю, залишаючись на піку особистої продуктивності</p>
+                <p class="mt-8 font-bold text-lg text-gray-600">- тренування не повторюються, забезпечуючи найвищий рівень залученості, кожне нове заняття це своєрідний челендж</p>
+                <p class="mt-8 font-bold text-lg text-gray-600">- розрахована на тренування в будь-яких умовах та локаціях</p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
-  </router-link>
+    <div class="flex justify-center">
+      <svg  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
+           stroke="currentColor" class="w-20 h-20 text-gray-950 animate-bounce bg-purple-800 rounded rounded-full">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 13.5 12 21m0 0-7.5-7.5M12 21V3" />
+      </svg>
+    </div>
+    <router-link to="/levels" class="">
+      <div class="box_animate flex hover:scale-125 transition-transform duration-300">
+        <div class="box animate rotate">
+          <div class="front animate movefront flex animate-fade-down" :style="{ background: frontColor }">
+            <img src="/public/vite.svg" alt="" class="p-6">
+          </div>
+          <div class="back animate moveback flex" :style="{ background: backColor }">
+            <img src="/public/vite.svg" alt="" class="p-6">
+          </div>
+        </div>
+      </div>
+    </router-link>
   </div>
 </template>
 
 <style scoped>
-.bg_images{
-  background-image: url("/public/images/mybg.webp");
-  min-width: 100%;
-  background-size: cover;
-  background-position: center;
-  background-repeat: no-repeat;
-}
 .box {
   position: relative;
   width: 500px;
